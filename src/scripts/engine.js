66feef178ec5aa0c54dd6aa63d50b710d0bda8b5
Douglas <douglas.nascimentosud21@gmail.com>
2024-10-12 02:06:28 -0300

    .

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,92 +1,108 @@
-
-
 const state = {
-    score: {
-        playerScore: 0,
-        computerScore: 0,
-        scoreBox: document.getElementById ("score_points")
-    },
-    cardSprites: {
-        avatar: document.getElementById ("card-image"),
-        name: document.getElementById ("card-name"),
-        type: document.getElementById ("card-type"),
-    },
-    fieldCards: {
-        player: document.getElementById ("player-fiel-card"),
-        computer: document.getElementById ("computer-fiel-card"),
-    },
-    actions: {
-        button: document.getElementById ("next-duel")
-    }, 
+  score: {
+    playerScore: 0,
+    computerScore: 0,
+    scoreBox: document.getElementById("score_points"),
+  },
+  cardSprites: {
+    avatar: document.getElementById("card-image"),
+    name: document.getElementById("card-name"),
+    type: document.getElementById("card-type"),
+  },
+  fieldCards: {
+    player: document.getElementById("player-field-card"),
+    computer: document.getElementById("computer-field-card"),
+  },
+  actions: {
+    button: document.getElementById("next-duel"),
+  },
 };
 
 //Enumeração
 const playersSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card"
+  player1: "player-cards",
+  computer: "computer-cards",
 };
 
 //Configuração dos Cards
-const pathImages = ".src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 const cardData = [
-    {
-        id: 0,
-        name: "Blue Eyes White Dragon",
-        type: "Paper",
-        img: `${pathImages} dragon.png`,
-        WinOf: [1],
-        LoseOf: [2]
-    },
-    {
-        id: 1,
-        name: "Dark Magician",
-        type: "Rock",
-        img: `${pathImages}magician.png`,
-        WinOf: [2],
-        LoseOf: [0],
-    },
-    {
-        id: 2,
-        name: "Exodia",
-        type: "Scissors",
-        img: `${pathImages}exodia.png`,
-        WinOf: [0],
-        LoseOf: [1]
-    },
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}dragon.png`,
+    WinOf: [1],
+    LoseOf: [2],
+  },
+  {
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}magician.png`,
+    WinOf: [2],
+    LoseOf: [0],
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    img: `${pathImages}exodia.png`,
+    WinOf: [0],
+    LoseOf: [1],
+  },
 ];
 
 //Pegar ID aleatoriamente
 async function getRandomCardId() {
-    const randomIndex = Math.floor (random () * cardData.length);
-    return cardData[randomIndex].id;
-};
+  const randomIndex = Math.floor(Math.random() * cardData.length);
+  return cardData[randomIndex].id;
+}
+
+//Criar imagens dos Cards
+async function createCardImage(IdCard, fieldSide) {
+  const cardImage = document.createElement("img");
+  cardImage.setAttribute("height", "100px");
+  cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+  cardImage.setAttribute("data-id", IdCard);
+  cardImage.classList.add("card");
+
+  if (fieldSide === playersSides.player1) {
+    cardImage.addEventListener("mouseover", () => {
+      drawSelectCard(IdCard);
+    });
+
+    cardImage.addEventListener("click", () => {
+      setCardField(cardImage.getAttribute("data-id"));
+    });
+  }
+
+  return cardImage;
+}
+
+async function drawSelectCard(index) {
+  state.cardSprites.avatar.src = cardData[index].img;
+  state.cardSprites.name.innerText = cardData[index].name;
+  state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
+}
 
 //Chamada das cartas
 async function drawCards(cardNumbers, fieldSide) {
-    for (let i = 0; i < cardNumbers; i++) {
-        const randomIdCard = await getRandomCardId ();
-        const cardImage = await createCardImage (randomIdCard, fieldSide);
+  for (let i = 0; i < cardNumbers; i++) {
+    const randomIdCard = await getRandomCardId();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
 
-        document.getElementById (fieldSide).appendChild(cardImage);
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
+    document.getElementById(fieldSide).appendChild(cardImage);
+  }
+}
 
 //Inicialização do jogo
-function init () {
-    drawCards (5, playerSides.player1)
-    drawCards (5, playerSides.computer)
-}; 
+function init() {
+  drawCards(5, playersSides.player1);
+  drawCards(5, playersSides.computer);
+}
 
-init ();
+init();
+
+
+//parei no implementando setcardfield
